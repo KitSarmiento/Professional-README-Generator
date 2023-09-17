a07077f5b3a4b9a021784206635311aa68e61360
KitSarmiento <ksarmiento020@gmail.com>
2023-09-17 18:34:56 -0400

    Add a function that will generate the license badge.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to generate the license badge
+function generateLicenseBadge(license) {
+  const licenseBadges = {
+    MIT: "https://img.shields.io/badge/License-MIT-yellow.svg",
+    "Apache-2.0": "https://img.shields.io/badge/License-Apache%202.0-blue.svg",
+    "GNU GPL v3": "https://img.shields.io/badge/License-GPLv3-blue.svg",
+  };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  const badgeURL = licenseBadges[license] || "";
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if (badgeURL) {
+    return `![${license} License](${badgeURL})\n`;
+  }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return "";
 }
-
-module.exports = generateMarkdown;
