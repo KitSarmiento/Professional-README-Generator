be2da590e91bd1ef2050f4f8d113284dcf693702
KitSarmiento <ksarmiento020@gmail.com>
2023-09-17 18:10:08 -0400

    function that will write a README file

--- index.js
+++ index.js
@@ -66,8 +66,16 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error("Error writing README.md:", err);
+    } else {
+      console.log("README.md has been successfully created!");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
