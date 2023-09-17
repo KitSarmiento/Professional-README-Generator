30a7d41fe810bdfed5a5360f1c04d0cc86a97607
KitSarmiento <ksarmiento020@gmail.com>
2023-09-17 18:39:09 -0400

    added a function that will generate anote what licesed the user is using.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,3 +14,16 @@ function generateLicenseBadge(license) {
 
   return "";
 }
+
+// Function to generate a note on what license the repository or user use.
+function generateLicenseNotice(license) {
+  const licenseNotices = {
+    MIT: "This project is licensed under the MIT License.",
+    "Apache-2.0": "This project is licensed under the Apache License 2.0.",
+    "GNU GPL v3": "This project is licensed under the GNU GPLv3.",
+  };
+
+  const licenseNotice = licenseNotices[license] || "";
+
+  return licenseNotice;
+}
